7b0c8ac09086e4054541aa14d0ffba0d753d53b4
belovd90@maol.ru <belovd90@mail.ru>
2022-07-30 15:17:32 +0300

    добавление наличия авторизации

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,7 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
 import { Question, Questions, UserAnswer } from '../types/question';
+import { AuthorizationStatus } from '../const';
 
 export const incrementStep = createAction('game/incrementStep');
 export const checkUserAnswer = createAction<{question: Question, userAnswer: UserAnswer}>('game/checkUserAnswer');
 export const resetGame = createAction('game/resetGame');
 export const loadQuestions = createAction<Questions>('data/loadQuestions');
+export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
